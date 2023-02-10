be154b745b7393595552ef8a424d072bce7a31e6
MScheiterle <scheiterlem@gmail.com>
2023-02-10 15:31:43 -0500

    Fixed .env and updated preview

--- src/firebase.js
+++ src/firebase.js
@@ -24,6 +24,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURE_ID,
 };
 
 const app = initializeApp(firebaseConfig);
