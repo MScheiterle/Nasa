d458c96e03c463e06961097ade4560722366d47c
MScheiterle <scheiterlem@gmail.com>
2023-04-12 15:32:45 -0400

    Projects done New themes next? Maybe work on one of the idea projects...

--- src/Constants.ts
+++ src/Constants.ts
@@ -13,74 +13,60 @@ export interface Project {
   status: string;
   description: string;
   timespan?: string;
-  rating?: string;
   githubLink?: string;
-  technologies?: Array<String>;
-  dateCreated: number; // Unix Mili Format
+  technologies?: Array<string>;
+  dateCreated?: number; // Unix Milli Format
 }
 
 export const projects: Project[] = [
   {
-    name: "Website",
+    name: "Simpl1f1ed.com",
     status: "current",
     description:
-      "Constantly working here to improve my front end design skill, adding themes and creating a dynamic content loading scheme to ensure that all themes work seamlessly with each other. This allows me a platform to display my talent and projects. Aswell as, make projects with a sophisticated back end using Firebase.",
+      "This is where I show off my mad front-end skills and create fancy themes that work so seamlessly with each other that even the best professional illusionists would be jealous. It's also where I store my top-secret stash of sloth memes.",
     timespan: "Constant",
-    rating: "6/10",
     githubLink: "https://github.com/MScheiterle/Simpl1f1ed.com",
-    technologies: [
-      "JS",
-      "HTML",
-      "CSS",
-      "SASS",
-      "TS",
-      "FireBase",
-      "React",
-    ],
+    technologies: ["JS", "HTML", "CSS", "SASS", "TS", "Firebase", "React"],
     dateCreated: 1681238336000,
   },
   {
     name: "Capstone Project",
     status: "finished",
     description:
-      "This project was meant to be for 3 people and take 6 weeks, my classmates chose to not do anything so I did it alone in half the time since they decided to tell me late they weren't going to participate. This is a basic CRUD app that allows a user to create tasks and publish them for others to see. The rating of 3/10 is from the harsh colors and bad design choices. The functionality was all there but it was build on python Flask. Proud I got it done though.",
+      "This project was an exercise in perseverance in the face of difficulty. Despite struggling with technical aspects and encountering difficulties with teammates who were unable or unwilling to help, I remained determined and committed to seeing the project through to the end. In the end, I completed the project on my own and was proud to have done it better and faster than most of the other teams.",
     timespan: "3 weeks",
-    rating: "3/10",
     githubLink: "https://github.com/MScheiterle/Capstone-Project",
     technologies: ["Python", "Flask", "CSS", "HTML", "JS"],
     dateCreated: 1680810698303,
   },
   {
-    name: "QuickMaths",
+    name: "SwiftSolve",
     status: "planned",
     description:
-      "This will be the foundation for a competative and practice math website, testing how fast people can do all forms of math problems. It will be hosted on this website until it is fully finished and in demand.",
+      "This project is like a personal trainer for your brain. It will test your math and reading skills in a fun and challenging way, just like how working out with a personal trainer is both fun and challenging, except without the sweating.",
     timespan: "",
-    rating: "",
     githubLink: "",
-    technologies: ["JS", "HTML", "CSS"],
+    technologies: ["JS", "HTML", "React", "Firebase", "SASS"],
     dateCreated: 1680810698303,
   },
   {
-    name: "Open Intel",
+    name: "OpenIntel",
     status: "planned",
     description:
-      "This software will allow access to all Open Source Intelligence (OSINT) gathering tools. I hope this becomes something that people can use more than the other white pages copy sites that have fake information. This will be difficult though...",
+      "A tool for all the Sherlock Holmes wannabes out there who want to get the inside scoop on everyone and everything. With this software, you'll have access to all the Open Source Intelligence (OSINT) gathering tools. Who knows what secrets you'll uncover? Probably nothing too exciting, but still worth a try.",
     timespan: "",
-    rating: "",
     githubLink: "",
-    technologies: ["JS", "HTML", "CSS"],
+    technologies: ["JS", "HTML", "React", "Firebase", "SASS"],
     dateCreated: 1680810698303,
   },
   {
-    name: "Image Comb",
+    name: "ImageComb",
     status: "planned",
     description:
-      "This is planned as an image encryption and tagging website. The hope is to have public and easy access to free images with accurate tagging and search.",
+      "A website that encrypts and tags images to make them easily searchable and shareable.",
     timespan: "",
-    rating: "",
     githubLink: "",
-    technologies: ["JS", "HTML", "CSS"],
+    technologies: ["JS", "HTML", "React", "Firebase", "SASS"],
     dateCreated: 1680810698303,
   },
 ];
